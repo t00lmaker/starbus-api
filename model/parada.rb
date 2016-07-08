12b9308b327af14fbf4780ba052f4c2fcb4b9573
Luan Pontes <luanpontes2@gmail.com>
2016-07-07 21:11:13 -0300

    Add ordenaçnao na lista de linhas de uma parada

--- model/parada.rb
+++ model/parada.rb
@@ -2,7 +2,7 @@ require "active_record"
 
 class Parada < ActiveRecord::Base
 
-  has_and_belongs_to_many :linhas
+  has_and_belongs_to_many :linhas , -> { order "codigo ASC" }
   has_one :reputation
 
   attr_accessor :dist
