d07a56be9965e4d1b7dc09678d00a8b8756c6dfc
Luan Pontes <luanpontes2@gmail.com>
2016-10-10 16:28:59 -0300

    Primeiro modelo de dashboard

--- starbus-web.rb
+++ starbus-web.rb
@@ -1,12 +1,23 @@
 require 'sinatra/base'
+require 'timerizer'
+
 module StarBus
-
   class Web < Sinatra::Base
-
     get "/" do
      puts " < landingpage >"
      erb :landingpage
     end
-
+    get "/dashboard" do
+      erb :dashboard
+    end
+    get "/snapshots" do
+      content_type :json
+      _snaps = Snapshot.where(data: 6.hour.ago..Time.now-3).order(:data)
+      @snaps = {}
+      _snaps.each do |s|
+        @snaps[s.data.strftime('%H:%M')] = JSON.parse(s.value)
+      end
+      @snaps.to_json
+    end
   end
 end
