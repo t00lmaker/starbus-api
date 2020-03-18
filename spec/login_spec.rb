bfd8598f7b11192d246cb29c138137d65c87f5d4
Luan Pontes <luanpontes2@gmail.com>
2020-03-18 00:06:09 -0300

    test  login

---
+++ spec/login_spec.rb
@@ -0,0 +1,16 @@
+
+describe :login do
+
+  it "POST / should be return a string" do
+    post '/v2/login', {application: "starbus", username: "root", password: ""}
+    expect_status(201)
+    expect_json_types(token: :string)
+  end
+
+  it "POST / should be return 401 when use or pass invalid" do
+    post '/v2/login', {application: "starbus", username: "invalid", password: ""}
+    expect_status(403)
+    post '/v2/login', {application: "starbus", username: "root", password: "invalid"}
+    expect_status(403)
+  end
+end
