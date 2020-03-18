2a427833e7e99a4017b5418012594c012d484fbf
Luan Pontes <luanpontes2@gmail.com>
2020-03-18 00:05:59 -0300

    test crud application

---
+++ spec/application_spec.rb
@@ -0,0 +1,21 @@
+
+describe :applications do
+
+  it "POST / should be create a new application." do
+    post '/v2/applications', {name: "newapp", description: "a new application"}, token_head
+    expect_status(201)
+    expect Application.all.select{|app| app.name == "newapp"}
+  end
+
+  it "GET / should return all user's applications" do
+    get '/v2/applications', token_head
+    expect_status(200)
+    expect_json_types(applications: :array_of_objects)
+  end
+
+  it "GET /:id should return return new the application with id" do
+    get '/v2/applications/1', token_head
+    expect_status(200)
+    expect_json('application.id', 1)
+  end
+end
