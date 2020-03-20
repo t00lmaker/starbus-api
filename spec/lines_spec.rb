e74fae882c9a3cf1b268b36e41ba838aad5e1a42
Luan Pontes <luanpontes2@gmail.com>
2020-03-20 01:09:50 -0300

    testes para lines

---
+++ spec/lines_spec.rb
@@ -0,0 +1,51 @@
+describe :lines do 
+ 
+  before(:all) do
+    truncate(Line)
+    Line.create(code: "505", description: "REDONDA", return: "PRACA BANDEIRA", origin: "REDONDA")
+    Line.create(code: "327", description: "RODOVIARIA", return: "UNIVERSIDADE", origin: "RODOVIARIA")
+    Line.create(code: "503", description: "ALTO DA RESSU", return: "mocambinho", origin: "ALTO")
+  end
+
+  context "GET /lines" do
+    it "when codes and search empty return all lines" do
+      get "/v2/lines", token_head
+      expect_status(200)
+      expect_json_types(lines: :array_of_objects)
+      expect_json_sizes(lines: 3)
+    end
+  
+    it "should be return all lines with codes in order asc" do
+      get "/v2/lines?codes[]=503&codes[]=327", token_head
+      expect_status(200)
+      expect_json_types(lines: :array_of_objects)
+      expect_json_sizes(lines: 2)
+      expect_json("lines.0", code: -> (code){ expect(code).to eq("327")})
+      expect_json("lines.1", code: -> (code){ expect(code).to eq("503")})
+    end
+
+
+    it "should be return all lines with term search in order asc" do
+      get "/v2/lines?search=universidade redonda", token_head
+      expect_status(200)
+      expect_json_types(lines: :array_of_objects)
+      expect_json_sizes(lines: 2)
+      expect_json("lines.0", code: -> (code){ expect(code).to eq("327")})
+      expect_json("lines.1", code: -> (code){ expect(code).to eq("505")})
+    end
+
+    it "should return empity when code not exist" do
+      get "/v2/lines?codes[]=5&codes[]=3", token_head
+      expect_status(200)
+      expect_json_types(lines: :array_of_objects)
+      expect_json_sizes(lines: 0)
+    end
+
+    it "should return empty when term not exist" do
+      get "/v2/lines?search=term", token_head
+      expect_status(200)
+      expect_json_types(lines: :array_of_objects)
+      expect_json_sizes(lines: 0)
+    end
+  end
+end
